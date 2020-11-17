fba73e4eef5014ddfb9435b29d62cd698fd3abf5
CREMA <crema@FAC1s-MacBookAir.local>
2020-11-17 14:13:28 +0900

    Add articles model, controller, views

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,44 @@
+class ArticlesController < ApplicationController
+	def index
+
+	end
+
+	def show
+
+	end
+
+	def new
+		@article = Article.new
+	end
+
+	def create
+		@article = Article.new(article_params)
+		if @article.save 
+			redirect_to @article
+		else 
+			render 'new'
+		end
+	end
+
+	def edit
+
+	end
+
+	def update
+
+	end
+
+	def destroy
+
+	end
+
+	private 
+
+	def find_article
+
+	end
+
+	def article_params
+		params.require(:article).permit(:title, :content)
+	end
+end
